d93d4d3cb51e92e8bccfd1fb7b47ffed48795fcb
SEYE Birane <b.seye@hi.org>
2025-08-19 18:29:50 +0200

    Ajout psycopg2 pour PostgreSQL sur

--- config/settings.py
+++ config/settings.py
@@ -4,13 +4,17 @@ import dj_database_url
 from dotenv import load_dotenv
 import environ
 
+env = environ.Env()
+environ.Env.read_env()  # charge le .env
+
+
 # ------------------------------
 # BASE & ENV
 # ------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 SECRET_KEY = os.getenv('SECRET_KEY', '8dgts#kd-6ge=ipv9)ke6n0uo_fjty8-awbu$zv3yvn(o7ey^(')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
@@ -69,19 +73,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ------------------------------
 # DATABASE
 # ------------------------------
-DATABASE_URL = os.getenv('DATABASE_URL')  # Render fournit DATABASE_URL
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
-else:
-    # fallback SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#DATABASE_URL = os.getenv('DATABASE_URL')  # Render fournit DATABASE_URL
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+#else:
+    # fallback SQLiteDATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}}
 
 # ------------------------------
 # PASSWORD VALIDATION
